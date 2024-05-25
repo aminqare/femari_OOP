43fc192778ff7adb595f0915b8da5886acfc35d5
MobinaKhosravani <mobina.khs83@gmail.com>
2024-05-25 20:03:13 +0330

    added Regexes and some view classes

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("this is a test");
-        //salam
-        //hello
-        //wusup dude this is whaaaaaat?
-        // mobina kheili dooset daramaaaaaaaaaaa
-        //lfohiuasnh;oidsjfio;aed
-        // this is Amin
+
     }
 
 }
