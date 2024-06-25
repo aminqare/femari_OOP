422290a2e35656d91647ec1e5d4acd549907fd8f
aminqare <amin6gh@gmail.com>
2024-06-25 15:46:56 +0330

    Finishing the Admin Menu pt.2

--- src/model/cards/cards.java
+++ src/model/cards/cards.java
@@ -4,18 +4,46 @@ public class cards {
     private String name;
     private double defence;
     private double attack;
+    private int duration;
     private double damage;
     private int level;
     private double upgradeCost;
-    private int duration;
 
-    public cards(String name, double defence, double attack, double damage, int level, double upgradeCost,int duration) {
+    public cards(String name, double defence, double attack, int duration, double damage, int level, double upgradeCost) {
         this.name = name;
         this.defence = defence;
         this.attack = attack;
+        this.duration = duration;
         this.damage = damage;
         this.level = level;
         this.upgradeCost = upgradeCost;
-        this.duration=duration;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getDefence() {
+        return defence;
+    }
+
+    public double getAttack() {
+        return attack;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public double getDamage() {
+        return damage;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public double getUpgradeCost() {
+        return upgradeCost;
     }
 }
